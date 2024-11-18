14f1a43fde700f205ac73831a0ad32e0eeee7d5b
Yamini6302 <129651557+Yamini6302@users.noreply.github.com>
2024-11-18 11:05:38 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './component/LoginPage';
-import RegisterPage from './component/RegisterPage';
-import HomePage from './component/HomePage';
-import StudentDetails from './component/StudentDetails'; 
-import TutorDetails from './component/TutorDetails'; 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './component/LandingPage'; 
+import LoginPage from './component/LoginPage'; 
+import RegisterPage from './component/RegisterPage'; 
+import StudentDetails from './component/StudentDetails';
+import TutorDetails from './component/TutorDetails';
+import TutorDashboard from "./component/TutorDashboard";
+import StudentDashboard from "./component/StudentDashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<HomePage />} />
         <Route path="/student-details" element={<StudentDetails />} />
         <Route path="/tutor-details" element={<TutorDetails />} />
+        <Route path="/tutor" element={<TutorDashboard />} /> 
+        <Route path="/student" element={<StudentDashboard />} />
       </Routes>
     </Router>
   );
