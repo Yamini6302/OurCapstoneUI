d12eed7091ca54383d9007c655e4f7a63bcf035b
Yamini6302 <129651557+Yamini6302@users.noreply.github.com>
2024-11-20 00:21:06 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './component/LandingPage'; 
+import LoginPage from './component/LoginPage'; 
+import RegisterPage from './component/RegisterPage'; 
+import StudentDetails from './component/StudentDetails';
+import TutorDetails from './component/TutorDetails';
+import TutorDashboard from "./component/TutorDashboard";
+import StudentDashboard from "./component/StudentDashboard";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/student-details" element={<StudentDetails />} />
+        <Route path="/tutor-details" element={<TutorDetails />} />
+        <Route path="/tutor" element={<TutorDashboard />} /> 
+        <Route path="/student" element={<StudentDashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
